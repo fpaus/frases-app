d59d0a0bcde9ff1e6404aaada20ae97a9a39beb1
Fabrizio Dante Pauselli <fabriziopauselli@gmail.com>
2023-01-31 12:54:02 -0300

    refactor: Context en App y no en main

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,15 @@ import './App.css';
 import FrasesAdder from './components/FrasesAdder';
 import FrasesList from './components/FrasesList';
 import Search from './components/Search';
+import { FrasesContextProvider } from './contexts/FrasesContext';
 
 function App() {
   return (
-    <>
+    <FrasesContextProvider>
       <Search />
       <FrasesAdder />
       <FrasesList />
-    </>
+    </FrasesContextProvider>
   );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { FrasesContextProvider } from './contexts/FrasesContext';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <FrasesContextProvider>
-      <App />
-    </FrasesContextProvider>
+    <App />
   </React.StrictMode>,
 );
