54792e01eb50468977eb9590fa54adbd2d23d6d0
Fabrizio Dante Pauselli <fabriziopauselli@gmail.com>
2023-01-31 11:27:55 -0300

    fix: agrega correctamente las frases

--- src/components/FrasesAdder/useFrasesAdder.ts
+++ src/components/FrasesAdder/useFrasesAdder.ts
@@ -1,17 +1,10 @@
-import {
-  ChangeEvent,
-  KeyboardEvent,
-  useCallback,
-  useContext,
-  useState,
-} from 'react';
+import { ChangeEvent, KeyboardEvent, useContext, useState } from 'react';
 import { FrasesContext } from '../../contexts/FrasesContext';
 
 export function useFrasesAdder() {
   const [fraseActual, setFraseActual] = useState('');
   const { dispatch } = useContext(FrasesContext);
-  const handleAdd = useCallback(() => {
-    console.log(fraseActual);
+  const handleAdd = () => {
     if (fraseActual.length > 0) {
       dispatch({
         type: 'ADD_FRASE',
@@ -21,19 +14,17 @@ export function useFrasesAdder() {
       });
       setFraseActual('');
     }
-  }, []);
-  const handleOnChange = useCallback((e: ChangeEvent<HTMLInputElement>) => {
-    console.log({
-      value: e.target.value,
-      trim: e.target.value?.trimStart() ?? '',
-    });
+  };
+  const handleOnChange = (e: ChangeEvent<HTMLInputElement>) => {
     setFraseActual(e.target.value?.trimStart() ?? '');
-  }, []);
-  const handleKeyDown = useCallback((event: KeyboardEvent) => {
+  };
+
+  const handleKeyDown = (event: KeyboardEvent) => {
     if (event.key === 'Enter') {
       handleAdd();
     }
-  }, []);
+  };
+
   return {
     fraseActual,
     handleAdd,
